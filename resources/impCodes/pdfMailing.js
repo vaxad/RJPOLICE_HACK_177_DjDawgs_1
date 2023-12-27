3d4c71e27753c358e4f1373bdefc0c764d1b958b
vaxad <varadprabhu111@gmail.com>
2023-12-27 21:58:03 +0530

    testing

--- resources/impCodes/pdfMailing.js
+++ resources/impCodes/pdfMailing.js
@@ -3,14 +3,14 @@ const sendMail = async () => {
     const send = true
     if (send) {
         console.log("Mail sending")
-        const htmlres = (await fetch("http://localhost:4001/mail",{
+        const htmlres = (await fetch("https://rakshakrita-v2.onrender.com/mail",{
             method: "POST",
             headers: { 'Content-Type': 'application/json' }
         }))    //to generate html from feedback analysis
         const htmlData = await htmlres.json()
         console.log(htmlData)
         const html = htmlData.html.join(" ")
-        const resp = (await fetch("http://localhost:4000/mail", {         // to generate pdf and send mail
+        const resp = (await fetch("https://rakshakrita-v1.onrender.com/mail", {         // to generate pdf and send mail
             method: 'POST', 
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ html: html })
